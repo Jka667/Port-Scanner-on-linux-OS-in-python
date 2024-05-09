7754ac0c8b9baa037a42ba4a7f6d48c34d47918e
Jonathan kassegne <127143809+Jka667@users.noreply.github.com>
2024-05-09 14:02:44 -0400

    Create Scanner.py

---
+++ Scanner.py
@@ -0,0 +1,38 @@
+import sys
+import socket
+from datetime import datetime
+
+# Define the target 
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 scanner.py <ip>")
+    sys.exit()
+
+# Create a default banner
+print("." * 50)
+print("Scanning target " + target)
+print("TIME STARTED: " + str(datetime.now()))
+
+try:
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+        print("Let's check port {}".format(port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
